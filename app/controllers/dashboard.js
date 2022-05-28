971d18d8f2ac7bdb213a1da60121ae7727638bf5
Candice Hall <hall.candice@outlook.com>
2022-05-28 17:33:48 -0400

    started working on api routes and regular routes

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models');
-// const withAuth = require('../../utils/auth');
+const withAuth = require('../../utils/auth');
 
 // Return all posts associated with the user
-router.get('/', (req, res) => {
+router.get('/', withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id
+    },
+    
+  })
   res.render('dashboard');
 });
 
--- app/controllers/search-routes.js
+++ app/controllers/search-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Category, Pet } = require('../models');
 
 router.get('/', (req, res) => {
   res.render('search');
