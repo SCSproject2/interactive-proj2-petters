4996a1ab92943faddb9ee9f6f2720c681f07fc5a
Csabbah <csabbah@ryerson.ca>
2022-05-28 18:09:14 -0400

    Created model associations in models/index

--- app/models/index.js
+++ app/models/index.js
@@ -1,1 +1,70 @@
 // Create the associations here
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+const Like = require('./Like');
+const Category = require('./Category');
+
+// ------- ------- User
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
+});
+
+// ------- ------- Post
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL',
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL',
+});
+
+Post.hasMany(Category, {
+  foreignKey: 'id',
+});
+
+// ------- ------- Vote
+Like.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
+});
+
+Like.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL',
+});
+
+// ------- ------- Comment
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL',
+});
+
+// ------- ------- Category
+Category.belongsTo(Post, {
+  foreignKey: 'post_ud',
+});
+
+module.exports = { User, Post, Comment, Like, Category };
