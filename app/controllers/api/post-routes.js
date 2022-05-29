c55b922ed8c1d5bc6ad78935dbf172e62107b3fa
Candice Hall <hall.candice@outlook.com>
2022-05-29 12:44:38 -0400

    finished put, post, and delete post-routes

--- app/controllers/api/post-routes.js
+++ app/controllers/api/post-routes.js
@@ -80,13 +80,61 @@ router.get('/:id', (req,res)=>{
 
 // Create a new post
 router.post('/', (req, res) => {
+Post.create({
+    title: req.body.title,
+    body: req.body.body,
+    user_id: req.session.user_id,
+    category_id: req.body.category_id,
+    image_url: req.body.image_url
+})
+.then(dbPostData=> res.json(dbPostData))
+.catch(err=> res.status(500).json(err));
+});
 
+// Update a post
+router.put('/:id', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title,
+            body: req.body.body,
+            category_id: req.body.category_id,
+            image_url: req.body.image_url
+        },
+        {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData=>{
+        if(!dbPostData){
+            res.status(404).json({message: 'No post found with this id'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err=>{
+        res.status(500).json(err);
+    })
 });
 
 // Delete a post
-router.delete('/:id', (req, res) => {});
-
-// Update a post
-router.put('/:id', (req, res) => {});
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
