117fcba786868f589e17c8e42011d3972ee966ca
Candice Hall <hall.candice@outlook.com>
2022-05-29 12:22:30 -0400

    created get routes for api post

--- app/controllers/api/post-routes.js
+++ app/controllers/api/post-routes.js
@@ -1,8 +1,87 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Post, User, Category } = require('../../models');
+
+//Get all posts
+router.get('/', (req, res)=>{
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'body'
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Category,
+                attributes: ['category_name']
+            }
+        ]
+    })
+    .then(dbPostData=> res.json(dbPostData))
+    .catch(err=> {
+        res.status(500).json(err);
+    });
+});
+
+//Get one post
+router.get('/:id', (req,res)=>{
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'body'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Category,
+                attributes: ['category_name']
+            }
+        ]
+
+    })
+    .then(dbPostData=>{
+        if(!dbPostData){
+            res.status(404).json({message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err=>{
+        res.status(500).json(err);
+    });
+});
 
 // Create a new post
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+
+});
 
 // Delete a post
 router.delete('/:id', (req, res) => {});
