545c486c87f7cddac1ab9139aafc72fbbc93e9d9
Candice Hall <hall.candice@outlook.com>
2022-06-01 12:54:22 -0400

    added featured-routes

--- app/controllers/featured-routes.js
+++ app/controllers/featured-routes.js
@@ -1,8 +1,62 @@
 const router = require('express').Router();
-const { Post, User, Comment } = require('../models');
+const { Post, Comment, User, Like, Category } = require('../models');
+const sequelize = require('../config/connection');
 
 router.get('/', (req, res) => {
-  res.render('featured');
-});
+    Post.findAll({
+      attributes: ['id', 'title', 'body', 'created_at', 'user_id', 'image_url',
+      [sequelize.literal('(SELECT COUNT(*) FROM `like` WHERE post.id = like.post_id)'), 'like_count']],
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name'],
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username'],
+          },
+        },
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Like,
+          attributes: ['user_id'],
+        },
+      ]
+    })
+      .then((dbPostData) => {
+          // let largestNum = dbPostData.reduce(
+          //   (num1, num2) =>
+          //   (num1 = num1 > num2.likes ? num1 : num2.likes),
+          // 0);
+          // console.log(largestNum);
 
-module.exports = router;
+
+        const posts1 = dbPostData.map((post) => post.get({ plain: true }));
+
+        posts1.sort(function(a,b){
+          return a.like_count - b.like_count;
+        });
+
+        posts1.reverse();
+  
+        // Returns the categories and their names
+        posts1.forEach((item) => {
+          console.log(item.categories);
+        });
+        const posts = posts1.slice(0, 5);
+        console.log(posts);
+  
+        res.render('featured', { posts });
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
+
+  module.exports = router;
