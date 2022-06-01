8a91808b86a298afce48bf955579076c9a237b17
Candice Hall <hall.candice@outlook.com>
2022-05-31 20:27:33 -0400

    added script to login.handlebars and updated addevenlistener to proper form names

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -59,5 +59,5 @@ async function loginFormHandler(event) {
         }
     }
 }
-document.querySelector('#login').addEventListener('submit', loginFormHandler);
-document.querySelector('#signup').addEventListener('submit', signupFormHandler);
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
