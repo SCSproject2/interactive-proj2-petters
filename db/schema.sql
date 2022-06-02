e8a7059c109bdedb07d5c544c69a8c3ebc05b72c
feven <feventsegai58@gmail.com>
2022-06-02 14:08:21 -0400

    mysql not working

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,34 @@
 DROP DATABASE IF EXISTS petters_socials;
 
 CREATE DATABASE petters_socials;
+
+creat a table user(
+    id(INT)auto-incrment primary key
+    email
+    username
+    password);
+ DROP DATABASE IF EXISTS pet;
+
+creat a table pet (
+    id (int)primary key
+    name
+    breed-type
+    animal-type 
+    age
+    color 
+    user-id 
+);
+DROP DATABASE IF EXISTS post;
+
+creat a table post (
+    id (int) auto_increament (primary key)
+    title(int)
+    body
+    pet-id(int));
+
+    DROP DATABASE IF EXISTS comment;
+
+    creat a table comment(
+        id (int) auto_increament (primery key)
+        post_id(INT)
+    creat a table likes creat a category
