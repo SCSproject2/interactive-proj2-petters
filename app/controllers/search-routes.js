27f3fed4a4c5ef0f547a2ee84aee98b2e7ede387
Joey Cos <joeycosentino24@gmail.com>
2022-06-02 16:12:55 -0400

    attempted search routes, added css and formatting to features page

--- app/controllers/search-routes.js
+++ app/controllers/search-routes.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+const res = require('express/lib/response');
 const { Post, User, Comment, Category, Pet } = require('../models');
+
 
 router.get('/', (req, res) => {
   res.render('search');
 });
+
+
+
 
 module.exports = router;
