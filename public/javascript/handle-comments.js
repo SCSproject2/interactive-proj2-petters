b1a922464d6b19d6c5e584dc920b653f2d092c75
Csabbah <csabbah@ryerson.ca>
2022-06-07 17:35:21 -0400

    Updated post styling in the featured-page

--- public/javascript/handle-comments.js
+++ public/javascript/handle-comments.js
@@ -4,103 +4,104 @@ const editBtn = document.querySelectorAll('.edit-comment');
 
 //create new comment on single post
 async function handleCommentForm(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const comment_text = document.querySelector('#comment-field').value.trim();
+  const comment_text = document.querySelector('#comment-field').value.trim();
 
-    const post_id = window.location.toString().split('/')
-    [
-        window.location.toString().split('/').length - 1
-    ].split('?')[0];
+  const post_id = window.location
+    .toString()
+    .split('/')
+    [window.location.toString().split('/').length - 1].split('?')[0];
 
-    if (comment_text) {
-        const response = await fetch('/api/comments', {
-            method: 'POST',
-            body: JSON.stringify({
-                comment_text,
-                post_id
+  if (comment_text) {
+    const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({
+        comment_text,
+        post_id,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (response.ok) {
-            document.location.reload();
-        } else {
-            alert(response.statusText);
-        }
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
     }
+  }
 }
 //submit edited comments
 async function confirmEdit(comment_text, comment_id) {
-    if (comment_text) {
-        const response = await fetch(`/api/comments/${comment_id}`, {
-              method: 'PUT',
-              body: JSON.stringify({
-                  comment_text
-                }),
-              headers: {
-                  'Content-Type': 'application/json'
-              }
-          });
-          if (response.ok) {
-              document.location.reload();
-          } else {
-              alert(response.statusText);
-          }
-      }
+  if (comment_text) {
+    const response = await fetch(`/api/comments/${comment_id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        comment_text,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
+    }
+  }
 }
 
 //edit comments user made
 function editComment(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const comment_id = event.target.getAttribute('data-comment-id');
+  const comment_id = event.target.getAttribute('data-comment-id');
 
-    const oldP = document.querySelector(`#comment-${comment_id}`);
-    const oldText = document.querySelector(`#comment-${comment_id}`).textContent;
-    const newText = document.createElement('textarea');
-    newText.value = oldText;
-    oldP.parentNode.replaceChild(newText, oldP);
+  const oldP = document.querySelector(`#comment-${comment_id}`);
+  const oldText = document.querySelector(`#comment-${comment_id}`).textContent;
+  const newText = document.createElement('textarea');
+  newText.value = oldText;
+  oldP.parentNode.replaceChild(newText, oldP);
 
-    const confirmBtn = document.getElementById(`confirm-comment-${comment_id}`);
-    confirmBtn.style.display = 'flex';
+  const confirmBtn = document.getElementById(`confirm-comment-${comment_id}`);
+  confirmBtn.style.display = 'flex';
 
-    const thisEditBtn = document.getElementById(`edit-comment-${comment_id}`);
-    thisEditBtn.style.display = 'none';
+  const thisEditBtn = document.getElementById(`edit-comment-${comment_id}`);
+  thisEditBtn.style.display = 'none';
 
-    confirmBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        let comment_text = newText.value.trim();
-        confirmEdit(comment_text, comment_id);
-
-        
-    })
+  confirmBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let comment_text = newText.value.trim();
+    confirmEdit(comment_text, comment_id);
+  });
 }
 
 async function deleteComment(event) {
-    event.preventDefault();
-    const comment_id = event.target.getAttribute('data-comment-id');
-    console.log(comment_id);
+  event.preventDefault();
+  const comment_id = event.target.getAttribute('data-comment-id');
+  console.log(comment_id);
 
-    if (comment_id) {
-        const response = await fetch(`/api/comments/${comment_id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        if (response.ok) {
-            document.location.reload();
-        } else {
-            alert(response.statusText);
-        }
+  if (comment_id) {
+    const response = await fetch(`/api/comments/${comment_id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
     }
+  }
 }
 
-btn.addEventListener('click', handleCommentForm);
-editBtn.forEach(editBtn => editBtn.addEventListener('click', editComment));
-deleteBtn.forEach(deleteBtn => deleteBtn.addEventListener('click', deleteComment));
-
+if (btn == null) {
+} else {
+  btn.addEventListener('click', handleCommentForm);
+  editBtn.forEach((editBtn) => editBtn.addEventListener('click', editComment));
+  deleteBtn.forEach((deleteBtn) =>
+    deleteBtn.addEventListener('click', deleteComment)
+  );
+}
