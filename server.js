b89ff1c4531429c1688d3158d53599e596e13c95
Csabbah <csabbah@ryerson.ca>
2022-06-08 18:29:09 -0400

    Minor changes made

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 const sequelize = require('./app/config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
