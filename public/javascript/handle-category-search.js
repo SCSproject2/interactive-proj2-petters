aec62200d1c8e665b9a651600935e6f7691afe7d
Joey Cos <joeycosentino24@gmail.com>
2022-06-09 17:42:28 -0400

    responsivness changed from bootstrap and percentages to css and pixels

--- public/javascript/handle-category-search.js
+++ public/javascript/handle-category-search.js
@@ -23,7 +23,7 @@ const renderPosts = (postsObject) => {
       clearBtn.style.display = 'unset';
       const newEl = document.createElement('div');
       newEl.innerHTML += `
-        <div id='single-post-wrapper' class='col-sm-12 col-md-6 col-xl-3'>
+        <div id='single-post-wrapper'>
           <a id='single-post' href='/post/${el.id}'>
             <div id='post-header' class='${el.category_name}'>
               <p id='post-category'>${el.category_name}</p>
