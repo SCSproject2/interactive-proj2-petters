f62957391c75aaa650d567852547849e3cb3567b
Csabbah <csabbah@ryerson.ca>
2022-06-12 11:47:03 -0400

    Made final CSS touches accross all project

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -32,7 +32,10 @@ window.onscroll = function () {
   scrollFunction();
 };
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
     scrollBtn.classList.add('active');
   } else {
     scrollBtn.classList.remove('active');
